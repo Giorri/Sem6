8fda3fc081bf20936936f8c427fa1bbc04739d30
M.Giorri <egorichev-max@mail.ru>
2022-09-09 13:38:02 +0300

    first commit

---
+++ DZ_taks43/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double FindPoint00(double numB1, double numK1, double numB2, double numK2) // Поиск X_0
+{
+    double x0 = (numB2 - numB1) / (numK1 - numK2);
+    return x0;
+}
+double FindPointY0(double x0, double numK1, double numB1) // Поиск Y_0
+{
+    double y0 = (numK1 * x0) + numB1;
+    return y0;
+}
+Console.WriteLine("Задайте переменные первой прямой y = k1 * x + b1: ");
+Console.Write("Введите k1: ");
+int line1_tg = int.Parse(Console.ReadLine());
+Console.Write("Введите b1: ");
+int line1_var = int.Parse(Console.ReadLine());
+Console.WriteLine("Задайте переменные второй прямой y = k2 * x + b2:");
+Console.Write("Введите k2: ");
+int line2_tg = int.Parse(Console.ReadLine());
+Console.Write("Введите b2: ");
+int line2_var = int.Parse(Console.ReadLine());
+if (line1_tg == line2_tg) Console.WriteLine("Прямые параллельные и никогда не пересекаются.");
+else
+{
+    double Poisk00 = FindPoint00(line1_var, line1_tg, line2_var, line2_tg); 
+    double PoiskY0 = FindPointY0(Poisk00, line1_tg, line1_var);
+    Console.WriteLine("Точка пересечения: ");
+    Console.Write("(");
+    Console.Write(String.Join("; ", Poisk00, PoiskY0));
+    Console.Write(")");
+}
---
+++ DZ_task41/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] VvodChisel(int size) // Массив ввода базы данных;
+{
+    int[] MassivChisel = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} значение: ");
+        MassivChisel[i] = int.Parse(Console.ReadLine());
+    }
+    return MassivChisel;
+}
+int[] ChetPositivNumber(int[] MassivChisel) // Массив подсчета положительных элементов;
+{
+    int[] sum = { 0 }; // !!!! Здесь, как и в прошлом ДЗ пытаюсь указать просто в 17 строке написать: int sum = 0; в 20: sum++; Но выдает тогда ошибку error CS0029 следующего плана: Не удается неявно преобразовать тип "int" в "int[]"
+    for (int i = 0; i < MassivChisel.Length; i++)
+    {
+        if (MassivChisel[i] > 0) sum[0]++;
+    }
+    return sum;
+}
+// Задаем размерность M нашегo;
+Console.Write("Задайте желаемую длину массива: ");
+int dlinaMassiva = int.Parse(Console.ReadLine());
+// Вызываем функцию для создания и заполнения массива;
+if (dlinaMassiva > 0)
+{
+    int[] MyVvodChisel = VvodChisel(dlinaMassiva);
+    Console.WriteLine("Ваш массив данных: ");
+    Console.Write(string.Join(" ", MyVvodChisel));
+    // Вызываем  функцию подсчета положительных чисел;
+    int[] SumPositivElements = ChetPositivNumber(MyVvodChisel);
+    Console.WriteLine();
+    Console.Write("Положительных элементов : ");
+    Console.WriteLine(String.Join(" ", SumPositivElements));
+}
+else
+{
+    Console.WriteLine("Ваш ввод не корректен");
+}
